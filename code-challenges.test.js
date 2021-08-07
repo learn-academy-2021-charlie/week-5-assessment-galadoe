ac74ff0a6a2c6d26b859fb9ae763c1b2ca9eb516
galadoe <kgaladoe@gmail.com>
2021-08-06 20:38:52 -0700

    still working on the challenge question

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -17,59 +17,98 @@
 
 // a) Create a test with an expect statement using the variables provided.
 
-describe('codedMess', () => {
-    test('returns the coded messages converted', () => {
+describe('codedMessage', () => {
 
+    test('should convert to coded message', () => {
+      var secretCodeWord1 = "Lackadaisical"
+      // Expected output: "L4ck4d41s1c4l"
+      var secretCodeWord2 = "Gobbledygook"
+      // Expected output: "G0bbl3dyg00k"
+      var secretCodeWord3 = "Eccentric"
+      // Expected output: "3cc3ntr1c"
+      expect(codedMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(codedMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(codedMessage(secretCodeWord3)).toEqual("3cc3ntr1c")
     })
-
 })
-
-var secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-var secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-var secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
 
 
 
 // b) Create the function that makes the test pass.
 
-//create function - str
-//split the string and assign it to a var
-//use a for loop or higher order function to iterate through it
-//conditional, if a,e,i,o,u replace with 4,3,1,0
-//return new arr with joined
+//create a function - str
+// split str and assign to var
+//iterate using for loop or higher order function
+//condition, if a,e,i,o,u replace with 4,3,1,0
+// return new arr and join the str
+
+const codedMessage = (str) => {
+  let newStr = []
+  let strSplit = str.split("")
+  for(let i = 0; i < strSplit.length; i++){
+    if(strSplit[i] === "a"){
+      strSplit[i] = 4
+      console.log(strSplit[i]);
+    }else if(strSplit[i] === "e"){
+      strSplit[i] = 3
+    }
+  }
+}
 
 // --------------------2) Create a function that takes in an array and returns all the words that contain the letter a.
 
 // a) Create a test with an expect statement using the variable provided.
 
+describe('letterA', () => {
+    test('should contain the letter a', () => {
+      var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
+      // Expected output: ["Apple", "Banana", "Orange"]
+      var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
+      // Expected output: ["Mango", "Apricot", "Peach"]
+      expect(letterA(arrayOfWords1)).toEqual(["Apple", "Banana", "Orange"])
+      expect(letterA(arrayOfWords2)).toEqual(["Mango", "Apricot", "Peach"])
+    })
+
+})
 
 
 
-var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
-// Expected output: ["Apple", "Banana", "Orange"]
-var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
-// Expected output: ["Mango", "Apricot", "Peach"]
 
 
 
 // b) Create the function that makes the test pass.
 
-
+const letterA = (arr) => {
+  let newArr = []
+  for(let i=0; i < arr.length; i++){
+    if(arr[i].includes('A') || arr[i].includes('a'))
+      return newArr.push(arr[i])
+  }
+  // return newArr
+}
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with an expect statement using the variable provided.
 
-var hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-var hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-var hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
+describe('fullHouse', () => {
+    test('should return if its a full house or not', () => {
+      var hand1 = [5, 5, 5, 3, 3]
+      // Expected output: true
+      var hand2 = [5, 5, 3, 3, 4]
+      // Expected output: false
+      var hand3 = [5, 5, 5, 5, 4]
+      // Expected output: false
+      expect(fullHouse(hand1)).toEqual(true)
+      expect(fullHouse(hand2)).toEqual(false)
+      expect(fullHouse(hand3)).toEqual(false)
+    })
+
+})
 
 
 
 // b) Create the function that makes the test pass.
+const fullHouse = (arr) => {
+  
+}
--- interview-questions.md
+++ interview-questions.md
@@ -7,53 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Rails follows an MVC pattern. What does that mean?
 
-  Your answer:
+  Your answer: From the little I understand and what I believe it is, it allows us to ruby in a certain order
 
-  Researched answer:
+  Researched answer: a pattern used to decouple user-interface (view), data(model), and application logic (controller). this pattern helps to achieve separation of concerns
 
 
 
 2. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: relational databases stores data that are related to each other
 
-  Researched answer:
+  Researched answer: types of database that stores and provides access to data points that are related to one another
 
 
 
 3. What is object-relational mapping?
 
-  Your answer:
+  Your answer: from what I know, orm allows us to convert data that are not compatible
 
-  Researched answer:
+  Researched answer: a programming technique for converting data between incompatible type systems using object-oriented programming languages
 
 
 
 4. What is a gem?
 
-  Your answer:
+  Your answer: when you are creating a ruby file, gem comes along with. Gem is a package manager for ruby
 
-  Researched answer:
+  Researched answer: is a package manager for the Ruby programming language that provides a standard format for distributing Ruby programs and libraries
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: To be honest, I need to familiarize myself with this one
 
-  Researched answer:
+  Researched answer: a special relational database table column designated to uniquely identify each table record
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: a route that provieds mapping from HTTP verbs to controller CRUD actions (create, read, update, delete)
 
-2. JSON:
+2. JSON: (JavaScript Object Notation) a lightweight data-interchange format
 
-3. ERB:
+3. ERB: (eRuby) a templating system that embeds Ruby into a text document. it is often used to embed ruby code in an HTML document
 
-4. Params:
+4. Params: you can specify a method parameter that takes a variable number of arguments
 
-5. API:
+5. API: (application programming interface) a connection between computers or between computer programs
